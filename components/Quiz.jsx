c106fd59112559f0bb53a9b0928818a36038d6e2
Mohsen37 <mohsenshokooh37@gmail.com>
2023-04-03 07:51:46 -0700

    add components with API and answers

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -1,5 +1,54 @@
+import { useState } from "react";
+
 function Quiz(props) {
-  return <div></div>;
+  const [choice, setChoice] = useState({
+    choice: "",
+  });
+  let answerList = props.quizItem.incorrect_answers.concat(
+    props.quizItem.correct_answer
+  );
+  const shuffleAnswer = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  };
+  shuffleAnswer(answerList);
+
+  function showAnswerHandle(event) {
+    const { value, name } = event.target;
+    if (value === props.quizItem.correct_answer) {
+      console.log("✅");
+    } else {
+      console.log("❌");
+    }
+  }
+
+  return (
+    <div className="quiz-item">
+      <h3>{props.quizItem.question}</h3>
+      <div className="wrapper">
+        {answerList?.map((ans, index) => (
+          <div className="answer-list" key={index}>
+            <div>
+              <input
+                type="radio"
+                onChange={showAnswerHandle}
+                name={props.quizItem.correct_answer}
+                value={ans}
+                id={ans}
+              />
+              <label className={`{}`} htmlFor={ans}>
+                {ans}
+              </label>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Quiz;
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import Quiz from "../components/Quiz";
 import "./App.css";
 
 function App() {
+  const [checkList, setCheckList] = useState({
+    0: false,
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+  });
+
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+      .then((res) => res.json())
+      .then((data) => setData(data.results));
+  }, []);
+
   return (
     <div className="App">
-      <Quiz num={1} />
-      <Quiz num={2} />
-      <Quiz num={3} />
-      <Quiz num={4} />
+      {data?.map((item, index) => (
+        <Quiz key={index} quizItem={item} />
+      ))}
+      <div className="result">
+        {data && (
+          <button onClick={showResultHandle} className="result-btn">
+            Check Answers
+          </button>
+        )}
+      </div>
     </div>
   );
 }
